b2ee12675e941a7f891301dbcf25aa0ddf60eb56
CNGAMERPGG <917166997@qq.com>
2021-07-28 02:54:19 +0800

    21/7/28

---
+++ music-client/src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="the-footer">
+        <p>关于 | 帮助 | 条款 | 帮助 |</p>
+        <p>Copyright ©2021</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'the-footer'
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../assets/css/the-footer.scss';
+</style>
